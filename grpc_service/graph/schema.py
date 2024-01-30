f94424a1c6d48b61305b4e0e9b330cff2bef5e16
Jeff Su <jeffdsu@gmail.com>
2024-01-29 20:21:21 -0800

    updaets with health check

--- grpc_service/graph/schema.py
+++ grpc_service/graph/schema.py
@@ -4,15 +4,16 @@ import grpc
 import todo_pb2
 import todo_pb2_grpc
 import os
-
-
+from fastapi import FastAPI
+import fastapi
+from fastapi.middleware.cors import CORSMiddleware
+from strawberry.asgi import GraphQL
 def get_todos():
 
     with grpc.insecure_channel(os.environ['TODO_SERVICE_BASE_URL']) as channel:
         stub = todo_pb2_grpc.ToDoStub(channel)
         response = stub.searchTodos(todo_pb2.Null(id="123"))
         return list(map(lambda tododata: Todo(message=tododata.message), response.todos))
-
 
 
 @strawberry.type
@@ -24,4 +25,20 @@ class Todo:
 class Query:
     searchTodos: typing.List[Todo] = strawberry.field(resolver=get_todos)
 
+
+
+
 schema = strawberry.Schema(query=Query)
+
+app = FastAPI()
+app.mount("/graphql", GraphQL(schema, debug=True))
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
